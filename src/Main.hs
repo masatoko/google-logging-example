5f8e98a8c9e6138295d59faa1b26ce5ca22544fa
masatoko <masatokodo@gmail.com>
2017-10-28 15:39:10 +0900

    for gce_instance

--- src/Main.hs
+++ src/Main.hs
@@ -4,7 +4,7 @@ module Main where
 
 import Control.Lens
 import Control.Exception
-import Control.Monad
+import Data.Monoid ((<>))
 import Data.Text (Text)
 import Network.Google as Google
 import Network.Google.Auth
@@ -27,35 +27,15 @@ pprint =
 
 getEnv :: IO (Text, Text, Text, Text, Text)
 getEnv = do
-  -- FIXME: Need to ask the environment what our project-id etc is for logging.
-  --
   -- Name of log should be something like:
-  --   projects/my-project-137423/logs/my-app
+  --   projects/project-id/logs/my-app
   --
   -- MonitoredResource should contain to simulate Google's fluentd:
   --   labels: {
-  --     cluster_name:  "cluster-1"
-  --     container_name:  "my-app"
+  --     project_id:  "project-id"
   --     instance_id:  "7937682100289506354"
-  --     namespace_id:  "default"
-  --     pod_id:  "my-app-3144516956-msvv1"
-  --     project_id:  "my-project-137423"
   --     zone:  "us-central1-a"
   --   }
-  --
-  -- Entry labels should look something like:
-  --
-  -- labels: {
-  --   compute.googleapis.com/resource_id:  "7937682100289506354"
-  --   compute.googleapis.com/resource_name:  "fluentd-cloud-logging-gke-my-cluster-1-default-pool-dabd30fd-wv"
-  --   compute.googleapis.com/resource_type:  "instance"
-  --   container.googleapis.com/cluster_name:  "my-cluster-1"
-  --   container.googleapis.com/container_name:  "my-app"
-  --   container.googleapis.com/instance_id:  "7937682100289506354"
-  --   container.googleapis.com/namespace_name:  "default"
-  --   container.googleapis.com/pod_name:  "my-app-3144516956-msvv1"
-  --   container.googleapis.com/stream:  "stdout"
-  -- }
   --
   manager <- newManager tlsManagerSettings
   projectId <- Metadata.getProjectId manager
@@ -65,15 +45,14 @@ getEnv = do
   zone <- Metadata.getZone manager
   return (projectId, description, hostname, instanceId, zone)
 
-logMsg :: Bool -> Text -> Text -> Text -> Text -> IO (Rs EntriesWrite)
-logMsg useEnv clusterName namespace logName msg = do
-  when useEnv $ do
-    (projectId, description, hostname, instanceId, zone) <- getEnv
-    print ("projectId" :: Text, projectId)
-    print ("description" :: Text, description)
-    print ("hostname" :: Text, hostname)
-    print ("instanceId" :: Text, instanceId)
-    print ("zone" :: Text, zone)
+logMsg :: Text -> Text -> IO (Rs EntriesWrite)
+logMsg logId msg = do
+  (projectId, description, hostname, instanceId, zone) <- getEnv
+  print ("projectId" :: Text, projectId)
+  print ("description" :: Text, description)
+  print ("hostname" :: Text, hostname)
+  print ("instanceId" :: Text, instanceId)
+  print ("zone" :: Text, zone)
 
   lgr <- newLogger Google.Debug stdout
   env <- newEnv <&> (envLogger .~ lgr) . (envScopes .~ loggingWriteScope)
@@ -91,16 +70,17 @@ logMsg useEnv clusterName namespace logName msg = do
   let
     entry = logEntry & leTextPayload ?~ msg
     entries = [entry]
+    logName = "projects/" <> projectId <> "/logs/" <> logId
     resourceLabels = monitoredResourceLabels $ HM.fromList
-      [ ("cluster_name", clusterName)
-      , ("namespace_id", namespace)
+      [ ("project_id", projectId)
+      , ("instance_id", instanceId)
+      , ("zone", zone)
       ]
-    resource = monitoredResource
-      & mrType ?~ "container"
+    resource = monitoredResource -- Required
+      & mrType ?~ "gce_instance"
       & mrLabels ?~ resourceLabels
-    labels = writeLogEntriesRequestLabels $ HM.fromList
-      [ ("container.googleapis.com/cluster_name", clusterName)
-      , ("container.googleapis.com/namespace_name", namespace)
+    labels = writeLogEntriesRequestLabels $ HM.fromList -- Optional
+      [ ("key", "value")
       ]
 
   runResourceT . runGoogle env $
@@ -118,9 +98,9 @@ main :: IO ()
 main = do
   args <- Env.getArgs
   case args of
-    [env, clusterName, namespace, logName, msg] ->
+    [logId, msg] ->
       handle (\e -> pprint ("exception" :: Text, e :: SomeException)) $ do
         putStrLn "Writing log message..."
-        result <- logMsg (env == "yes") (T.pack clusterName) (T.pack namespace) (T.pack logName) (T.pack msg)
+        result <- logMsg (T.pack logId) (T.pack msg)
         pprint ("result" :: Text, result)
-    _ -> putStrLn "usage: google-log env cluster-name namespace log-name message"
+    _ -> putStrLn "usage: google-log log-id message"
